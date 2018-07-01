e86560f0ab9c1ac125f86e9b3a64da81a8f8b142
Scott Checko <schecko@sfu.ca>
2018-07-01 00:23:24 -0700

    fixed test

--- polls/models.py
+++ polls/models.py
@@ -10,6 +10,8 @@ class Question(models.Model):
         return self.question_text
 
     def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 class Choice(models.Model):
