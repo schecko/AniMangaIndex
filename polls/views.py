59bc92659f2563420619822661dcd3769a73cd90
Scott Checko <schecko@sfu.ca>
2018-06-30 23:52:11 -0700

    added voting to tutorial

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
-from django.http import HttpResponse, Http404
-from .models import Question
+from django.shortcuts import render, get_object_or_404, reverse
+from django.http import *
+from .models import Question, Choice
 from django.template import loader
 
 
@@ -12,10 +12,22 @@ def detail(request, questionId):
     return render(request, 'polls/detail.html', { 'question': question })
 
 def results(request, questionId):
-    return HttpResponse("You're looking at the results of question %s." % questionId)
+    question = get_object_or_404(Question, pk=questionId)
+    return render(request, 'polls/results.html', { 'question': question })
 
 def vote(request, questionId):
-    return HttpResponse("You're voting on question %s." % questionId)
+    question = get_object_or_404(Question, pk=questionId)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except(KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'errorMessage': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes = 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('results', args=(question.id,)))
 
 def index(request):
     questionList = Question.objects.order_by('-pub_date')[:5]
