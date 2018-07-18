8457408eb677bb768ec36f95e075c2aafb2a0e9c
Scott Checko <schecko@sfu.ca>
2018-07-18 01:01:20 -0700

    working on ui

--- animanga/main/views.py
+++ animanga/main/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from .models import *
+
+from .buildDB import fillDB
 
 def index(request):
-    return HttpResponse("Hello World. You're at the manga index")
+	contentList = Content.objects.all()
+
+	#if not contentList:
+	#	fillDB()
+
+	template = loader.get_template('main/index.html')
+	context = {
+		'contentList': contentList
+	}
+	return HttpResponse(template.render(context, request))
 
 def contentDetail(request, contentID):
 	return HttpResponse("Hello detailed world: %s" % contentID)
