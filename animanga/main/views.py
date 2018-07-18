1a628f96c3fefd5aa4dc26685d2da6ddd4b763d2
Scott Checko <schecko@sfu.ca>
2018-07-17 23:11:25 -0700

    starting views

--- animanga/main/views.py
+++ animanga/main/views.py
@@ -3,3 +3,9 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello World. You're at the manga index")
+
+def contentDetail(request, contentID):
+	return HttpResponse("Hello detailed world: %s" % contentID)
+
+def creatorDetail(request, creatorID):
+	return HttpResponse("Hello creator detailed world %s" % creatorID)
