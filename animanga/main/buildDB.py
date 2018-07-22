76bd86064bf77c247c9eb9e4631d99928d14b7b7
etonk <etonk@sfu.ca>
2018-07-21 17:29:55 -0700

    Fixed bug and updated table names
    Table name modified:
    Hires -> Hire
    Licenses -> License
    Creates -> Create
    
    Added html template for creator page
    Modified View to output author info

---
+++ animanga/main/buildDB.py
@@ -0,0 +1,208 @@
+from main.models import *
+from django.db import connection
+
+def fillDB():
+	cursor = connection.cursor()
+	
+	print ("inside fillDB")
+	# CREATE TABLE main_user(
+	  # userID     INT PRIMARY KEY,
+	  # privileges INT,
+	  # password   VARCHAR(255),
+	  # email      VARCHAR(255)
+	# );
+	#Inserting data into User table
+	userInsert = ['INSERT INTO main_user(userID,privileges,password,email) VALUES (0,0,123,"etonk@sfu.ca")',
+				'INSERT INTO main_user(userID,privileges,password,email) VALUES (1,1,123,"colinc@sfu.ca")',
+				'INSERT INTO main_user(userID,privileges,password,email) VALUES (2,1,123,"lindaj@sfu.ca")',
+				'INSERT INTO main_user(userID,privileges,password,email) VALUES (3,1,123,"scottc@sfu.ca")',
+				'INSERT INTO main_user(userID,privileges,password,email) VALUES (4,0,123,"test@sfu.ca")']
+	
+	i = 0
+	for contents in userInsert:
+		cursor.execute(str(userInsert[i]))
+		i+=1
+		result = cursor.fetchall()
+		
+	# CREATE TABLE main_studio(
+	  # name    VARCHAR(255) PRIMARY KEY,
+	  # founded DATE
+	# );
+	#Inserting data into Studio table	
+	studioInsert = ['INSERT INTO main_studio(name,founded) VALUES ("White Fox","2007-01-01")',
+					'INSERT INTO main_studio(name,founded) VALUES ("Studio Pierrot","1979-01-01")',
+					'INSERT INTO main_studio(name,founded) VALUES ("Studio 3Hz","2013-01-01")',
+					'INSERT INTO main_studio(name,founded) VALUES ("Ufotable","1997-01-01")']
+	
+	i = 0
+	for contents in studioInsert:
+		cursor.execute(str(studioInsert[i]))
+		i+=1
+		result = cursor.fetchall()
+	
+	# CREATE TABLE main_content(
+	  # contentID INT PRIMARY KEY,
+	  # type      VARCHAR(255),
+	  # title     VARCHAR(255),
+	  # complete  INT,
+	  # rating    INT,
+	  # date      DATE,
+	  # genre     VARCHAR(255),
+	  # source    VARCHAR(255)
+	# );
+	#Inserting data into Content table
+	contentInsert = ['INSERT INTO main_content(contentID,type,title,complete,rating,date,genre,source) VALUES (0,"Anime","Steins;Gate",1,9,"2018-04-12","Mystery","Manga")',
+					'INSERT INTO main_content(contentID,type,title,complete,rating,date,genre,source) VALUES (1,"Anime","Tokyo Ghoul",1,7,"2018-04-03","Action","Manga")',
+					'INSERT INTO main_content(contentID,type,title,complete,rating,date,genre,source) VALUES (2,"Anime","Sword Art Online",0,7,"2018-04-08","Fantasy","Book")',
+					'INSERT INTO main_content(contentID,type,title,complete,rating,date,genre,source) VALUES (3,"Manga","Fate/Zero",1,8,"2010-12-29","Action","Book")',
+					'INSERT INTO main_content(contentID,type,title,complete,rating,date,genre,source) VALUES (4,"Book","Fate/Zero",1,7,"2006-12-29","Action","Book")',
+					'INSERT INTO main_content(contentID,type,title,complete,rating,date,genre,source) VALUES (5,"Anime","Fate/Zero",1,9,"2011-10-01","Action","Manga")']
+	i = 0
+	for contents in contentInsert:
+		cursor.execute(str(contentInsert[i]))
+		i+=1
+		result = cursor.fetchall()
+		
+	# CREATE TABLE main_creator(
+	  # creatorID INT PRIMARY KEY,
+	  # birthday  DATE,
+	  # gender    INT,
+	  # name      VARCHAR(255)
+	# );
+	#Inserting data into Creator table	
+	creatorInsert = ['INSERT INTO main_creator(creatorID,birthday,gender,name) VALUES (0,"1971-01-02",0,"Kenichi Kawamura")',
+					'INSERT INTO main_creator(creatorID,birthday,gender,name) VALUES (1,"1969-01-01",0,"Jukki Hanada")',
+					'INSERT INTO main_creator(creatorID,birthday,gender,name) VALUES (2,"1970-07-03",0,"Chiyomaru Shikura")',
+					'INSERT INTO main_creator(creatorID,birthday,gender,name) VALUES (3,"1985-12-28",0,"Sui Ishida")',
+					'INSERT INTO main_creator(creatorID,birthday,gender,name) VALUES (4,"1978-06-22",0,"Shuhei Morita")',
+					'INSERT INTO main_creator(creatorID,birthday,gender,name) VALUES (5,"1980-07-24",0,"Chuji Mikasano")',
+					'INSERT INTO main_creator(creatorID,birthday,gender,name) VALUES (6,"1974-06-29",0,"Masayuki Sakoi")',
+					'INSERT INTO main_creator(creatorID,birthday,gender,name) VALUES (7,"1972-01-01",0,"Keiichi Sigsawa")',
+					'INSERT INTO main_creator(creatorID,birthday,gender,name) VALUES (8,"1968-03-29",0,"Yosuke Kuroda")',
+					'INSERT INTO main_creator(creatorID,birthday,gender,name) VALUES (9,"1972-12-20",0,"Gen Urobuchi")',
+					'INSERT INTO main_creator(creatorID,birthday,gender,name) VALUES (10,"1973-08-28",0,"Takashi Takeuchi")',
+					'INSERT INTO main_creator(creatorID,birthday,gender,name) VALUES (11,"1973-01-20",0,"Ei Aoki")',
+					'INSERT INTO main_creator(creatorID,birthday,gender,name) VALUES (12,"1967-02-15",0,"Akihiko Yoshida")']
+	
+	i = 0
+	for contents in creatorInsert:
+		cursor.execute(str(creatorInsert[i]))
+		i+=1
+		result = cursor.fetchall()
+	
+	# CREATE TABLE main_license(
+	  # contentID INT PRIMARY KEY,
+	  # id    VARCHAR(255),
+	  # publisher VARCHAR(255),
+	  # FOREIGN KEY (studio) references main_studio(studio),
+	  # FOREIGN KEY (contentID) references main_content(contentID)
+
+	# );
+	#Inserting data into License table
+	licenseInsert = ['INSERT INTO main_license(contentID_id,studio_id,publisher) VALUES (0,"White Fox","Funimation")',
+					'INSERT INTO main_license(contentID_id,studio_id,publisher) VALUES (1,"Studio Pierrot","Funimation")',
+					'INSERT INTO main_license(contentID_id,studio_id,publisher) VALUES (2,"Studio 3Hz","Aniplex of America")',
+					'INSERT INTO main_license(contentID_id,studio_id,publisher) VALUES (3,"Ufotable","Type-Moon")']
+	
+	i = 0
+	for contents in licenseInsert:
+		cursor.execute(str(licenseInsert[i]))
+		i+=1
+		result = cursor.fetchall()
+	
+	# CREATE TABLE main_hire(
+	  # id  VARCHAR(255) PRIMARY KEY,
+	  # creator INT,
+	  # FOREIGN KEY(studio) references main_studio(studio),
+	  # FOREIGN KEY (creator) references main_creator(creatorID)
+	# );
+	#Inserting data into Hire table	
+	hireInsert = ['INSERT INTO main_hire(studio_id,creator_id) VALUES ("White Fox",0)',
+				'INSERT INTO main_hire(studio_id,creator_id) VALUES ("White Fox",1)',
+				'INSERT INTO main_hire(studio_id,creator_id) VALUES ("White Fox",2)',
+				'INSERT INTO main_hire(studio_id,creator_id) VALUES ("Studio Pierrot",3)',
+				'INSERT INTO main_hire(studio_id,creator_id) VALUES ("Studio Pierrot",4)',
+				'INSERT INTO main_hire(studio_id,creator_id) VALUES ("Studio Pierrot",5)',
+				'INSERT INTO main_hire(studio_id,creator_id) VALUES ("Studio 3Hz",6)',
+				'INSERT INTO main_hire(studio_id,creator_id) VALUES ("Studio 3Hz",7)',
+				'INSERT INTO main_hire(studio_id,creator_id) VALUES ("Studio 3Hz",8)',
+				'INSERT INTO main_hire(studio_id,creator_id) VALUES ("Ufotable",9)',
+				'INSERT INTO main_hire(studio_id,creator_id) VALUES ("Ufotable",10)',
+				'INSERT INTO main_hire(studio_id,creator_id) VALUES ("Ufotable",11)',
+				'INSERT INTO main_hire(studio_id,creator_id) VALUES ("Ufotable",12)']
+	
+	i = 0
+	for contents in hireInsert:
+		cursor.execute(str(hireInsert[i]))
+		i+=1
+		result = cursor.fetchall()
+		
+	# CREATE TABLE main_create(
+	  # content INT PRIMARY KEY,
+	  # creator INT,
+	  # role    VARCHAR(255),
+	  # FOREIGN KEY (creator) references main_creator(creatorID),
+	  # FOREIGN KEY (content) references main_content(contentID)
+	# );
+	#Inserting data into Create table	
+	createInsert = ['INSERT INTO main_create(content_id,creator_id,role) VALUES (0,0,"Director")',
+					'INSERT INTO main_create(content_id,creator_id,role) VALUES (0,1,"Animator")',
+					'INSERT INTO main_create(content_id,creator_id,role) VALUES (0,2,"Author")',
+					'INSERT INTO main_create(content_id,creator_id,role) VALUES (1,3,"Author")',
+					'INSERT INTO main_create(content_id,creator_id,role) VALUES (1,4,"Director")',
+					'INSERT INTO main_create(content_id,creator_id,role) VALUES (1,5,"Animator")',
+					'INSERT INTO main_create(content_id,creator_id,role) VALUES (2,6,"Director")',
+					'INSERT INTO main_create(content_id,creator_id,role) VALUES (2,7,"Author")',
+					'INSERT INTO main_create(content_id,creator_id,role) VALUES (2,8,"Animator")',
+					'INSERT INTO main_create(content_id,creator_id,role) VALUES (3,9,"Author")',
+					'INSERT INTO main_create(content_id,creator_id,role) VALUES (4,9,"Author")',
+					'INSERT INTO main_create(content_id,creator_id,role) VALUES (5,9,"Author")',
+					'INSERT INTO main_create(content_id,creator_id,role) VALUES (3,10,"Artist")',
+					'INSERT INTO main_create(content_id,creator_id,role) VALUES (5,11,"Director")',
+					'INSERT INTO main_create(content_id,creator_id,role) VALUES (5,12,"Animator")']
+	
+	i = 0
+	for contents in createInsert:
+		cursor.execute(str(createInsert[i]))
+		i+=1
+		result = cursor.fetchall()
+		
+	# CREATE TABLE VolumeSeason(
+		# contentID INT PRIMARY KEY, 
+		# num       INT,
+		# title     VARCHAR(255),
+		# FOREIGN KEY (contentID) REFERENCES main_content(contentID)
+	# );
+	#Inserting data into VolumeSeason table	
+	volumeSeasonInsert = ['INSERT INTO main_volumeSeason(id,contentID_id,num,title) VALUES (0,0,1,"Steins;Gate 0")',
+						'INSERT INTO main_volumeSeason(id,contentID_id,num,title) VALUES (1,1,3,"Tokyo Ghoul:re")',
+						'INSERT INTO main_volumeSeason(id,contentID_id,num,title) VALUES (2,2,4,"Sword Art Online Alternative: Gun Gale Online")',
+						'INSERT INTO main_volumeSeason(id,contentID_id,num,title) VALUES (3,3,0,"Fate/Zero")',
+						'INSERT INTO main_volumeSeason(id,contentID_id,num,title) VALUES (4,4,5,"Fate/Zero")',
+						'INSERT INTO main_volumeSeason(id,contentID_id,num,title) VALUES (5,5,1,"Fate/Zero")']
+	
+	i = 0
+	for contents in volumeSeasonInsert:
+		cursor.execute(str(volumeSeasonInsert[i]))
+		i+=1
+		result = cursor.fetchall()
+	
+		# CREATE TABLE main_favoritecontent(
+	  # id INT,
+	  # contentID INT,
+	  # PRIMARY KEY (userID, ContentID), 
+	  # FOREIGN KEY (userID) REFERENCES Users (userID),
+	  # FOREIGN KEY (contentID) REFERENCES main_content(contentID)
+	# );
+	#Inserting data into FavoriteContent table	
+	favoriteContentInsert = ['INSERT INTO main_favoritecontent(userID_id,contentID_id) VALUES (0,0)',
+							'INSERT INTO main_favoritecontent(userID_id,contentID_id) VALUES (1,1)',
+							'INSERT INTO main_favoritecontent(userID_id,contentID_id) VALUES (2,1)',
+							'INSERT INTO main_favoritecontent(userID_id,contentID_id) VALUES (3,3)',
+							'INSERT INTO main_favoritecontent(userID_id,contentID_id) VALUES (4,5)']
+	
+	i = 0
+	for contents in favoriteContentInsert:
+		cursor.execute(str(favoriteContentInsert[i]))
+		i+=1
+		result = cursor.fetchall()
--- animanga/main/views.py
+++ animanga/main/views.py
@@ -1,15 +1,35 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 from main.models import *
-from main.init import *
+from main.buildDB import *
 
 def index(request):
     return HttpResponse("Hello World. You're at the manga index")
 
 def contentDetail(request, contentID):
+	contentList = Content.objects.all()
+	if not contentList:
+		fillDB()
+
 	data = Content.objects.filter(contentID=contentID)
 	return HttpResponse("Content info for %s (contentID = %s)" % (data[0].title, contentID))
 
 def creatorDetail(request, creatorID):
-	data = Creator.objects.filter(creatorID=creatorID)
-	return HttpResponse("Creator info for %s (contentID = %s)" % (data[0].name, creatorID))
+	creatorList = Creator.objects.all()
+	if not creatorList:
+		fillDB()
+	
+	creatorData = Creator.objects.raw('SELECT creatorID, name, gender, birthday FROM main_creator WHERE creatorID = %s' % creatorID)
+	contentList = Content.objects.raw('SELECT * FROM main_create A, main_content B WHERE A.content_id = B.contentID AND A.creator_id = %s' % creatorID)
+
+	template = loader.get_template('creator/creator.html')
+	context = {
+		'creatorName': creatorData[0].name,
+		'creatorGender': "Female" if creatorData[0].gender else "Male",
+		'creatorBday': creatorData[0].birthday,
+		'contentList': contentList
+	}
+	return HttpResponse(template.render(context, request))
+	
+	
